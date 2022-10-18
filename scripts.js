8e666a5b28f9e6bd8eca416f4996d1446bf439ba
kingsleycj <primekings.kc@gmail.com>
2022-10-18 15:04:26 +0100

    further refactoring

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
+// declaring global variables
+/*
 var html = '';
 var red;
 var green;
 var blue;
 var rgbColor;
-
+*/
 
 // longer procedure
 
@@ -67,9 +69,9 @@ var rgbColor;
 // rgbColor = "rgb(" + red + "," + green + "," + blue + ")";
 // html += '<div style="background-color: ' + rgbColor + '"></div>';
 
-
 // refactoring to simplify the code
-for (var randomColors = 1 ; randomColors <= 7; randomColors ++){
+/*
+for (var randomColors = 1 ; randomColors <= 10; randomColors ++){
     red = Math.floor(Math.random() * 256);
     green = Math.floor(Math.random() * 256);
     blue = Math.floor(Math.random() * 256);
@@ -78,3 +80,28 @@ for (var randomColors = 1 ; randomColors <= 7; randomColors ++){
 }
 
 document.write(html);
+*/
+
+// further refactoring
+var html = "";
+var rgbColor;
+
+function randomRGB() {
+  return Math.floor(Math.random() * 255);
+}
+function randomColor() {
+  var color = "rgb(";
+  color += randomRGB() + ",";
+  color += randomRGB() + ",";
+  color += randomRGB() + ")";
+  return color;
+}
+function print(message) {
+  document.write(message);
+}
+for (var i = 0; i <= 1000; i++) {
+  rgbColor = randomColor();
+  html += '<div style="background-color: ' + rgbColor + '"></div>';
+}
+
+print(html);
