f583afcc7db37daa35adad1c1c7550395769f07d
kingsleycj <primekings.kc@gmail.com>
2022-10-18 23:16:40 +0100

    js Quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,16 +1,27 @@
 var questions = [
-    ['How many states are in the United States?', '50'],
-    ['How many continents are there?', '3'],
-    ['How many letters of the alphabet do we have?', '27']
+    ['How many states are in the United States?', 50],
+    ['How many continents are there?', 7],
+    ['How many letters of the alphabet do we have?', 27]
 ];
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var html;
+
 
 function print(message) {
     document.write(message);
 }
 
-while ( true ) {
-    quiz = prompt(questions.indexOf(0));
-    quiz = quiz.parseInt();
-    if (quiz === questions.indexOf[0][1]) {
-        
+for (var i = 0; i < questions.length; i++) {
+    question = questions[i][0];
+    answer = questions[i][1];
+    response = parseInt(prompt(question));
+    if (response === answer ) {
+        correctAnswers += 1;
+    }
 }
+
+html = "You got " + correctAnswers + " question(s) right";
+print(html);
