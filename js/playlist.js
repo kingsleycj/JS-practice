72547cbc5a7478a58de5245f45e2354afdfaa460
kingsleycj <primekings.kc@gmail.com>
2022-10-18 20:05:25 +0100

    Arrays in JS

--- js/playlist.js
+++ js/playlist.js
@@ -1,4 +1,37 @@
-var playList = [];
-playList.push('I Did It My Way');
-playList.push('Respect', 'Imagine');
-printList(playList);
+// adding items to an array
+// var playList = [];
+// playList.push('I Did It My Way');
+// playList.push('Respect', 'Imagine');
+// playList.unshift('Born to Run');
+// playList.unshift('Louie Louie', 'Maybellene');
+// printList(playList);
+
+// removing items from an array
+/*
+var numbers = [1,2,3,4,5,6];
+var firstItem = numbers.shift();
+var lastItem = numbers.pop();
+
+console.log('first Item is ' + firstItem);
+console.log('last item is ' + lastItem);
+*/
+
+var playList = [
+    'I Did It My Way',
+    'Respect',
+    'Imagine',
+    'Born to Run',
+    'Louie Louie',
+    'Maybellene'
+];
+
+function print(message){
+    document.write(message);
+}
+
+function printList(list){
+    var listHTML = '<ol>';
+    for (var i = 0; i < list.length; i++) {
+        
+    }
+}
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,5 +1,6 @@
-// let nth_most_rate = (array, nthRarestTerm) => {
-//   return array.filter((n) => n == nthRarestTerm).length;
-// };
-
-// console.log(nth_most_rate([1, 2, 3, 4, 3, 3, 5], 3));
+var playList = [];
+// playList.push('I Did It My Way');
+// playList.push('Respect', 'Imagine');
+// playList.unshift('Born to Run');
+// playList.unshift('Louie Louie', 'Maybellene');
+// printList(playList);
