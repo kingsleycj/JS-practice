f7ec1494332edb3393f2b966cd23cd4f144073d6
kingsleycj <primekings.kc@gmail.com>
2022-10-21 16:53:41 +0100

    editing students database

--- js/students.js
+++ js/students.js
@@ -1,26 +1,31 @@
 var students = [
     { 
-        Name: 'Kingsley CJ', Track: 'Backend Development', Achievements: 300, Points: 1265 
+        Name: 'Kingsley CJ', 
+        Track: 'Backend Development', 
+        Achievements: 300, 
+        Points: 1265 
     }, 
     { 
-        Name: 'Michael', Track: 'Frontend Development', Achievements: 270, Points: 1163 
+        Name: 'Michael King', 
+        Track: 'Frontend Development', 
+        Achievements: 270, 
+        Points: 1163 
     }, 
     { 
-        Name: 'David Nweze' , Track: 'Ui/Ux Design' , Achievements: 350, Points: 1470 
+        Name: 'David Nweze' , 
+        Track: 'Ui/Ux Design' , 
+        Achievements: 350, 
+        Points: 1470 
     }, 
     { 
-        Name: 'Favour', Track: 'Frontend Development', Achievements: 250, Points: 1160 
+        Name: 'Ukamaka Stephanie', 
+        Track: 'Frontend Development', 
+        Achievements: 350, Points: 1460 
     }, 
     { 
-        Name: 'Ghydion' , Track: 'Full-Stack Development' , Achievements: 500 , Points: 1790 
+        Name: 'Ghydion Akunana' , 
+        Track: 'Full-Stack Development' , 
+        Achievements: 500 , 
+        Points: 1790 
     },
 ];
-// var html;
-
-//     html = "These are the list of 5 students and their records:";
-
-// for (prop in students){
-//     html += ( students[prop]);
-// }
-
-// print(html)
