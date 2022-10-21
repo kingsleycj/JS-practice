6776048ce3fde9f191904f9cc2e7097b6fd28ab7
kingsleycj <primekings.kc@gmail.com>
2022-10-21 18:48:00 +0100

    building a search mechanismfor the database

--- js/student_report.js
+++ js/student_report.js
@@ -7,25 +7,28 @@ function print(message) {
   outputDiv.innerHTML = message;
 }
 
+
+
 while (true) {
   search = prompt(
     'Search student records: type a name [Kingsley] (or type "quit" to end the search'
   );
-  search = search.toLowerCase();
-  if (search === "quit") {
+  if (search === null || search.toLowerCase() === "quit") {
     break;
-  } else if (search === student.Name[i]) {
-    print();
-  }
+    }
+    for (var i = 0; i < students.length; i++) {
+      student = students[i];
+      if ( student.name === search){
+
+      }
+      message += "<h2>Student: " + student.Name + "</h2>";
+      message += "<p>Track: " + student.Track + "</p>";
+      message += "<p>Achievements: " + student.Achievements + "</p>";
+      message += "<p>Points: " + student.Points + "</p>";
+    }
 }
 
-for (var i = 0; i < students.length; i++) {
-    student = students[i];
-    message += '<h2>Student: ' + student.Name + '</h2>';
-    message += '<p>Track: ' + student.Track + '</p>';
-    message += '<p>Achievements: ' + student.Achievements + '</p>';
-    message += '<p>Points: ' + student.Points + '</p>';
-}
+
 
 
 print(message);
--- js/students.js
+++ js/students.js
@@ -1,29 +1,29 @@
 var students = [
     { 
-        Name: 'Kingsley CJ', 
+        Name: 'Kingsley', 
         Track: 'Backend Development', 
         Achievements: 300, 
         Points: 1265 
     }, 
     { 
-        Name: 'Michael King', 
+        Name: 'Michael', 
         Track: 'Frontend Development', 
         Achievements: 270, 
         Points: 1163 
     }, 
     { 
-        Name: 'David Nweze' , 
+        Name: 'David' , 
         Track: 'Ui/Ux Design' , 
         Achievements: 350, 
         Points: 1470 
     }, 
     { 
-        Name: 'Ukamaka Stephanie', 
+        Name: 'Ukamaka', 
         Track: 'Frontend Development', 
         Achievements: 350, Points: 1460 
     }, 
     { 
-        Name: 'Ghydion Akunana' , 
+        Name: 'Ghydion' , 
         Track: 'Full-Stack Development' , 
         Achievements: 500 , 
         Points: 1790 
