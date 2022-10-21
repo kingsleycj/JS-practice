dc1cf527d87f8245e196b146a69369bfaf412d5a
kingsleycj <primekings.kc@gmail.com>
2022-10-21 19:04:48 +0100

    modifying the search mechanism

--- js/student_report.js
+++ js/student_report.js
@@ -7,7 +7,7 @@ function print(message) {
   outputDiv.innerHTML = message;
 }
 
-function getStudentReport ( student ) {
+function getStudentReport (student) {
     var report = "<h2>Student: " + student.Name + "</h2>";
     report += "<p>Track: " + student.Track + "</p>";
     report += "<p>Achievements: " + student.Achievements + "</p>";
@@ -16,17 +16,17 @@ function getStudentReport ( student ) {
 }
 
 while (true) {
-  search = prompt(
-    'Search student records: type a name [Kingsley] (or type "quit" to end the search'
-  );
+  search = prompt('Search student records: type a name [Kingsley] (or type "quit" to end the search');
   if (search === null || search.toLowerCase() === "quit") {
     break;
     }
     for (var i = 0; i < students.length; i++) {
       student = students[i];
-      if ( student.name === search){
+      if ( student.Name === search){
         message = getStudentReport( student);
         print(message);
+      } else if ( student.Name !== search){
+        alert('User data not available for this student', search);
       }
     }
 }
