34bb75d68947e86da84d7eeb495e9d7a13c829f6
kingsleycj <primekings.kc@gmail.com>
2022-10-23 11:05:05 +0100

    fix

--- js/student_report.js
+++ js/student_report.js
@@ -7,26 +7,28 @@ function print(message) {
   outputDiv.innerHTML = message;
 }
 
-function getStudentReport (student) {
-    var report = "<h2>Student: " + student.Name + "</h2>";
-    report += "<p>Track: " + student.Track + "</p>";
-    report += "<p>Achievements: " + student.Achievements + "</p>";
-    report += "<p>Points: " + student.Points + "</p>";
-    return report;
+function getStudentReport(student) {
+  var report = "<h2>Student: " + student.Name + "</h2>";
+  report += "<p>Track: " + student.Track + "</p>";
+  report += "<p>Achievements: " + student.Achievements + "</p>";
+  report += "<p>Points: " + student.Points + "</p>";
+  return report;
 }
 
 while (true) {
-  search = prompt('Search student records: type a name [Kingsley] (or type "quit" to end the search)');
+  search = prompt(
+    'Search student records: type a name [Kingsley] (or type "quit" to end the search)'
+  );
   if (search === null || search.toLowerCase() === "quit") {
     break;
-    }
-    for (var i = 0; i < students.length; i++) {
-      student = students[i];
-      if ( student.Name === search){
-        message = getStudentReport( student);
-        print(message);
-       } // else if ( student.Name !== search ){
-        // print('User data not available for this student');
-      }
-    }
+  }
+  for (var i = 0; i < students.length; i++) {
+    student = students[i];
+    if (student.Name === search) {
+      message = getStudentReport(student);
+      print(message);
+    } // else if ( student.Name !== search ){
+    // print('User data not available for this student');
+  }
 }
+ 
