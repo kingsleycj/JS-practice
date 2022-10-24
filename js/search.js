f332fd921e786fa81fa09d7816a6db98790f36d9
kingsleycj <primekings.kc@gmail.com>
2022-10-24 10:25:26 +0100

    using iteration to sum two arrays

--- js/search.js
+++ js/search.js
@@ -39,12 +39,37 @@ const productOfXY = X.map((value, index) => value * Y[index]);
 console.log('sum of X and Y is: ' + sumOfXY);
 console.log('product of X and Y is: ' + productOfXY);
 */
-
+/*
 const x = [1, 2, 3, 4, 5];
 const y = [6, 7, 8, 9, 10];
+const sumOfXY = 0;
+const productOfXY = 0;
 
-sumOfXY = [x[0] + y[0], x[1] + y[1], x[2] + y[2], x[3] + y[3], x[4] + y[4]];
-productOfXY = [x[0] * y[0], x[1] * y[1], x[2] * y[2], x[3] * y[3], x[4] * y[4]];
+for (var i = 0; i < x.length; i++ &&  j = x[i]; j < y.length; i++) {
+    sumOfXY += y[i];
+    productOfXY += y[i];
+}
+// sumOfXY = [x[0] + y[0], x[1] + y[1], x[2] + y[2], x[3] + y[3], x[4] + y[4]];
+// productOfXY = [x[0] * y[0], x[1] * y[1], x[2] * y[2], x[3] * y[3], x[4] * y[4]];
 
 console.log(sumOfXY);
 console.log(productOfXY);
+*/
+
+
+
+
+ var X = [1, 2, 3, 4, 5];
+ var Y = [6, 7, 8, 9, 10];
+var sum = 0;
+var sumOfXY = [];
+var productOfXY = [];
+for (var i = 0; i < X.length; i++) {
+  sum = X[i] + Y[i];
+  product = X[i] * Y[i];
+  sumOfXY.push(sum);
+  productOfXY.push(product);
+}
+
+console.log(sumOfXY)
+console.log(productOfXY)
