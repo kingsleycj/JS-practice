be99a630c1224afa7b02c093226c0e000005a2a7
kingsleycj <primekings.kc@gmail.com>
2022-10-26 13:39:25 +0100

    arrow function syntax

---
+++ Arrow Function Syntax/01_no_arguments.js
@@ -0,0 +1,12 @@
+const name = 'Andrew';
+
+const sayName = () => {
+    const message = 'My name is ' + name;
+    console.log(message)
+}
+
+sayName();
+
+const sayBye = () => {
+    console.log("Bye" + name);
+}
