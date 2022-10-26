20ec69c89b17194f12a9dbcbd8710aa376a5cba5
kingsleycj <primekings.kc@gmail.com>
2022-10-26 13:51:31 +0100

    functionally the same

---
+++ Arrow Function Syntax/04_functionally_the_same.js
@@ -0,0 +1,19 @@
+// Function declaration
+
+function divide(a,b) {
+    return a / b; 
+}
+
+// function expression
+
+const divide2 = function(a,b) {
+    return a / b;
+}
+
+// arrow function
+const divide3 = (a,b) => {
+    return a / b;
+}
+
+// concise arrow function expression
+const divide4 = (a,b) => a / b;
