9de929a80c4444aafdc3327a3c2b0b3013583517
kingsleycj <primekings.kc@gmail.com>
2022-11-01 00:42:39 +0100

    getting all children of a node with children

--- js/app.js
+++ js/app.js
@@ -147,7 +147,7 @@ const listUl = document.querySelector('ul');
 const addItemInput = document.querySelector("input.addItemInput");
 const addItemButton = document.querySelector("button.addItemButton");
 // const removeItemButton = document.querySelector("button.removeItemButton"); //REMOVING REMOVE BUTTON QUERY SELECTOR
-
+const lis = listUl.children;
 
 // utilizing event bubbling for better efficient event targeting
 /*
@@ -164,16 +164,23 @@ for (let i = 0; i < listItems.length; i++) {
 }
 */
 
-function attachListItemButtons () {
+function attachListItemButtons (li) {
     let up = document.createElement('button');
     up.className = 'up';
     up.textContent = 'Up';
+    li.appendChild(up);
     let down = document.createElement('button');
     down.className = 'down';
     down.textContent = 'Down';
+    li.appendChild(down);
     let remove = document.createElement('button');
     remove.className = 'remove';
     remove.textContent = 'Remove';
+    li.appendChild(remove);
+}
+
+for (let i = 0; i < lis.length; i++) {
+    attachListItemButtons(lis[i]);
 }
 
 listUl.addEventListener("click", (event) => {
@@ -234,6 +241,7 @@ addItemButton.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li','button');
     li.textContent = addItemInput.value;
+    attachListItemButtons(li);
     ul.appendChild(li);
     addItemInput.value = "";
 });
