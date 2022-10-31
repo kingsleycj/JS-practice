e7c1f18b5fd5c5020c82eb249869c9d4d17c15fc
kingsleycj <primekings.kc@gmail.com>
2022-10-31 02:09:51 +0100

    using nexElementSibling to build a list app

--- js/app.js
+++ js/app.js
@@ -167,9 +167,27 @@ for (let i = 0; i < listItems.length; i++) {
 listUl.addEventListener("click", (event) => {
     if (event.target.tagName == 'BUTTON') {
         // event.target.textContent = event.target.textContent.toUpperCase();
-        let li = event.target.parentNode;
-        let ul = li.parentNode;
-        ul.removeChild(li);
+        if (event.target.className == 'remove') {
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+        }
+        if (event.target.className == 'up') {
+            let li = event.target.parentNode;
+            let prevLi = li.previousElementSibling;
+            let ul = li.parentNode;
+            if (prevLi) {
+                ul.insertBefore(li, prevLi);
+            }
+        }
+        if (event.target.className == 'down') {
+            let li = event.target.parentNode;
+            let nextLi = li.nextElementSibling;
+            let ul = li.parentNode;
+            if (nextLi) {
+                ul.insertBefore(nextLi, li);
+            }
+        }
     }
 });
 
