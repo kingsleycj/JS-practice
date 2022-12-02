01ff9cdc24cfd629e697c41b52313810034bfe0a
kingsleycj <primekings.kc@gmail.com>
2022-12-02 13:21:05 +0100

    checkout ui

--- js/eCommerceConsole.js
+++ js/eCommerceConsole.js
@@ -1,58 +1,74 @@
 var catalog = [
-    ["iPhone 11", 320],
-    ["Samsung Galaxy s10", 250],
-    ["Xiaomi Redmi note 9", 200],
-    ["Tecno Camon 16", 160],
-    ["Infinix Note 10", 90],
-    ["LG nexus", 220],
+  ["iPhone 11", 320],
+  ["Samsung Galaxy s10", 250],
+  ["Xiaomi Redmi note 9", 200],
+  ["Tecno Camon 16", 160],
+  ["Infinix Note 10", 90],
+  ["LG nexus", 220],
 ];
 
+// var logging = [
+//     {
+//         name: "iPhone 11",
+//         price: 320
+//     },
+//     {
+//         name: "ip"
+//     }
+// ]
+//
+// logging[0].name
+
 function print(message) {
-    console.log(message);
+  console.log(message);
 }
 
 function printList(items) {
-    var list = "\n***************************************\n";
-    for (var i = 0; i < items.length; i++) {
-        list += "\n[" + items[i][0] + " costs " + "$" + items[i][1] + "]\n";
-    }
-    list += "\n***************************************\n";
-    print(list);
+  var list = "\n***************************************\n";
+  for (var i = 0; i < items.length; i++) {
+    list += "\n[" + items[i][0] + " costs " + "$" + items[i][1] + "]\n";
+  }
+  list += "\n***************************************\n";
+  print(list);
 }
 
 var cart = [];
 var r1 = require("readline");
 var prompts = r1.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 prompts.question("Enter your name: ", function (username) {
-    print(`Hi ${username}, Welcome to Kingsley\'s store`);
-    print(`\nHere are the list and prices of our Phone Catalog: `);
-    printList(catalog);
-    prompts.question(
-        "\nSelect the device you'd like to add to your shopping cart:  ",
-        function (selectedItem) {
-            if (catalog[0].includes(selectedItem) ) {
-                print(`${selectedItem} has been added to the shopping cart\n`);
-                cart.push(selectedItem);
-                print("Your shopping cart : \n " + cart);
-            } else {
-                print(selectedItem + " is not in stock");
-            }
-            prompts.question("\nWould you like to add more to your shopping cart? ", function (moreItems) {
-                if (catalog[0].includes(moreItems) ) {
-                    print(`${moreItems} has been added to the shopping cart\n`);
-                    cart.push(moreItems);
-                    print("Your shopping cart : \n ");
-                    print(cart);
-                 }  // else if (moreItems === null) {
-                //     // cart.pop(moreItems);
-                //     // print(cart);
-                //     print("Thank You for shopping from our store")
-                // }
-            })
+  print(`Hi ${username}, Welcome to Kingsley\'s store`);
+  print(`\nHere are the list and prices of our Phone Catalog: `);
+  printList(catalog);
+  prompts.question(
+    "\nSelect the device you'd like to add to your shopping cart:  ",
+    function (selectedItem) {
+      for (let i = 0; i < catalog.length; i++) {
+        if (catalog[i][0].includes(selectedItem)) {
+          print(`${selectedItem} has been added to the shopping cart\n`);
+          cart.push(selectedItem);
+          print("Your shopping cart : \n " + cart);
+        } else {
+          print(selectedItem + " is not in stock");
         }
-    );
+        prompts.question(
+          "\nWould you like to add more to your shopping cart? ",
+          function (moreItems) {
+            if (catalog[i][0].includes(moreItems) && moreItems !== null) {
+              print(`${moreItems} has been added to the shopping cart\n`);
+              cart.push(moreItems);
+              print("Your shopping cart : \n ");
+              print(cart);
+            } else if (moreItems === null) {
+              // cart.pop(moreItems);
+              // print(cart);
+              print("Thank You for shopping from our store");
+            };
+          }
+        );
+      }
+    }
+  );
 });
-
--- js/new.js
+++ js/new.js
@@ -3,14 +3,14 @@ var prompts = r1.createInterface(process.stdin, process.stdout);
 prompts.question("Enter your Name: ", function(username){
     prompts.question("Enter your age: ", function(userage){
         prompts.question("Enter your Contact Address: ", function(address){
-            prompts.question("Enter your phone number: (make sure its 11 digits) ", function(mobile){
+            prompts.question("Enter your phone number\n(make sure its 11 digits): ", function(mobile){
                 var message = "";
-                if (mobile.indexOf(mobile) > 10){
+                if (mobile.length >= 10){
                     message = "\n\n Thank You! " + username + "\n\n Your details have been stored,  You can make your payment and expect delivery in 2 day(s)" +
-                    "\n\n Username                 : " + username + 
-                    "\n\n Age                           : " + userage + 
-                    "\n\n Contact Address                  : " + address + 
-                    "\n\n Phone Number           : " + mobile ;
+                    "\n\n Username               : " + username + 
+                    "\n\n Age                   : " + userage + 
+                    "\n\n Contact Address       : " + address + 
+                    "\n\n Phone Number          : " + mobile ;
                 } else {
                     message = "Your Phone number is " + mobile + " , and its not up to 11 digits, please restart and input 11  digits of your phone number ";
                 }
