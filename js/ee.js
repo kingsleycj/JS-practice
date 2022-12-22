828fe873ce31d49b00bf202289f76c90abfc1244
kingsleycj <primekings.kc@gmail.com>
2022-12-22 16:55:03 +0100

    backing up: OOP work

--- js/ee.js
+++ js/ee.js
@@ -1,48 +1,177 @@
-/*
-let catalog = [
-  ["iPhone X", 200],
-  ["iPhone 11", 325],
-  ["iPhone 11 Pro Max", 450],
-  ["iPhone Xs", 250],
-  ["iPhone Xs Max", 320],
-  ["iPhone 12", 470],
-];
-*/
+const prompt = require("prompt-sync")({ sigint: true });
 
-var catalog = [
-   [ "iPhone 11", 22],
-   [ "Samsung Galaxy S10", 230],
-    ["Google One Plus 6", 230],
-    ["iPhone 12 pro max", 230],
-   [ "iPhone 12 mini", 230],
-   [ "Samsung Galaxy S21", 230],
-   [ "LG Nexus", 230],
-    ["Google One Plus 5", 230],
-    ["Xiaomi Redmi Note 6", 230],
-];
+const cart = [];
+const products = new Map();
 
-function print(message) {
-    console.log(message);
-}
+products.set("Drinks", [
+  ["Pepsi", 500, 1],
+  ["Coke", 500, 1],
+  ["Fanta", 500, 1],
+]);
+products.set("Food", [
+  ["Rice", 2000, 1],
+  ["Swallow", 5000, 1],
+  ["Pie", 7000, 1],
+]);
+products.set("Furniture", [
+  ["Table", 15000, 1],
+  ["Tv", 50000, 1],
+  ["Chair", 8000, 1],
+]);
+products.set("Cosmetics", [
+  ["Cream", 6000, 1],
+  ["Scrub", 3000, 1],
+  ["Perfume", 4000, 1],
+]);
+products.set("Clothes", [
+  ["Trouser", 7500, 1],
+  ["Shirts", 5000, 1],
+  ["Shoes", 20000, 1],
+]);
+products.set("Car", [
+  ["Honda", 500000, 1],
+  ["Camry", 1000000, 1],
+  ["Corolla", 750000, 1],
+]);
+products.set("Appliances", [
+  ["Laptop", 500000, 1],
+  ["Phone", 200000, 1],
+  ["Tablet", 400000, 1],
+]);
 
-var cart = [];
-var r1 = require("readline");
-var prompts = r1.createInterface(process.stdin, process.stdout);
-prompts.question("Enter your Name: ", function (username) {
-    print(`Welcome ${username} to Kingsley\'s store`);
-    print(`\nHere are the list and prices of our Phone Catalog: \n\n${catalog}`);
-    prompts.question(
-        "\nSelect the device you'd like to add to your shopping cart:  ",
-        function (selectedItem) {
-            if (catalog.indexOf(selectedItem) > -1) {
-                print(`${selectedItem} has been added to the shopping cart\n`);
-                var carts = cart.push(selectedItem);
-                print('Your shopping cart : \n ');
-                print(carts);
-            } else {
-                print(selectedItem + " is not in stock");
-            }
-        }
+const displayCategory = () => {
+  let category = "";
+  let count = 1;
+  for (const key of products.keys()) {
+    category += `${count}. ${key}\n`;
+    count++;
+  }
+  return category;
+};
+
+const displayProducts = (num = 0) => {
+  let count = 1;
+  if (num < 0 || num > 7) {
+    return `Please input a valid Aisle or Product`;
+  } else {
+    const key = Array.from(products.keys())[num - 1];
+    const values = products.get(key);
+    let toStringProduct = "";
+    values.forEach((value) => {
+      toStringProduct += `\n${count}.\t${value[0]}\t\t${value[2]}\t\t  ${value[1]}`;
+      count++;
+    });
+    return `\tProducts\tQuantity\tAmount` + toStringProduct;
+  }
+};
+
+const calculateTotal = () => {
+  let totalAmount = 0;
+  cart.forEach((product) => {
+    totalAmount += product[2] * product[1];
+  });
+  return totalAmount;
+};
+
+const calculateDiscount = (total = calculateTotal()) => {
+  if (total > 200000) {
+    return 20;
+  } else if (total > 150000) {
+    return 15;
+  } else if (total > 100000) {
+    return 10;
+  } else if (total > 50000) {
+    return 5;
+  } else if (total > 40000) {
+    return 3;
+  } else if (total > 30000) {
+    return 2;
+  } else {
+    return 0;
+  }
+};
+
+const calculateTax = (total = calculateTotal()) => {
+  return total * 0.1;
+};
+
+const displayCart = () => {
+  let toString = "";
+  cart.forEach((product) => {
+    toString += `\n${product[0]}\t\t${1}\t\t  ${product[1]}`;
+  });
+  return `Product\t\tQuantity\tAmount` + toString;
+};
+
+const manageOrders = () => {
+  console.log(
+    "\n*********************************************************************************"
+  );
+  console.log(
+    "********************************Genesys Groceries********************************"
+  );
+  console.log(
+    "*********************************************************************************\n"
+  );
+  const name = prompt(
+    "Please input your name to gain access to your personal cart: "
+  ).toUpperCase();
+  console.log(
+    `\n\tWelcome ${name}!!!\n\n\tSo pleased to have you here ${name}.\n`
+  );
+  console.log(
+    `Hey ${name}, buy at least goods worth 30000 naira to qualify for discount of up to 20%.\n`
+  );
+  while (true) {
+    console.log(
+      "What action would you like to perform?\nOptions: \n\ta) Add to cart\n\tb) Remove from cart\n\tc) Checkout"
     );
-});
-
+    const option = prompt().toLowerCase();
+    switch (option) {
+      case "a":
+        console.log(`\n${displayCategory()}`);
+        console.log("Choose a category to shop for between: 1 and 7 above\n\t");
+        const category = prompt();
+        console.log(
+          displayProducts(category) +
+            "\n\nChoose an item to add to your cart between: 1 and 3 above\n\t"
+        );
+        const itemToAdd = prompt();
+        const key = Array.from(products.keys())[category - 1];
+        cart.push(products.get(key)[itemToAdd - 1]);
+        break;
+      case "b":
+        console.log(
+          `\nChoose an item to delete from your cart between: 1 and ${cart.length} above\n\t`
+        );
+        const itemToDelete = prompt();
+        cart.splice(itemToDelete - 1, 1);
+        break;
+      case "c":
+        console.log("Thank you for shopping with us.\n");
+        const address = prompt("Please input your address: ");
+        return console.log(
+          "\n*************************************RECEIPT*************************************\n" +
+            displayCart() +
+            `\n\nTotal purchase: ${calculateTotal()}\nTax: ${calculateTax()}\nDiscount:${
+              calculateDiscount() * 0.01 * calculateTotal()
+            }\n\n` +
+            `Amount to be paid = ${
+              calculateTotal() * calculateDiscount() + calculateTax()
+            }` +
+            `\n\nPlease expect your package within 5 working days to be delivered to ${address}\n\n`
+        );
+      default:
+        continue;
+    }
+    console.log(
+      "\n" +
+        "\n**************************************CART**************************************\n" +
+        displayCart() +
+        `\n\n\tTotal: ${calculateTotal()}` +
+        `\nYou are eligible for ${calculateDiscount()}% discount\n`
+    );
+    prompt("Press enter to continue!\n");
+  }
+};
+manageOrders();
--- js/new.js
+++ js/new.js
@@ -156,12 +156,167 @@ const ezeh = new Customer()
 
 
 
-class withBackbone {
-    warmBlooded = true
+// An Abstract class where all other classes will be referenced from
+class Animal {
+    #animalName;
+    #phylum;
 
+    // constructor function that will accept the animalName and Phylum as parameters
+    constructor(animalName, phylum) {
+        if (this.constructor === Animal) {
+        throw new Error("This class cannot be called or instantiated");
+        }
+        this.#animalName = animalName;
+        this.#phylum = phylum;
+    }
+// a method to return the name of the animal
+    getAnimalName() {
+        return this.#animalName;
+    }
+
+    setAnimalName() {
+        this.#animalName = animalName;
+    }
+    getPhylum() {
+        this.#phylum;
+    }
+
+    warmBlooded() {
+        this.warmBlooded = true;
+    }
+
+    coldBlooded() {
+        this.coldBlooded = true;
+    }
+
+    listProperties() {
+        throw new Error("This method cannot be implemented");
+    }
+
+    withBackbone() {
+        this.withBackbone = true;
+    }
+
+    withoutBackbone() {
+        this.withoutBackbone = true;
+    }
 }
 
 
-class withoutBackbone {
+class Aves extends Animal {
+    constructor(animalName, phylum) {
+        super(animalName, phylum);
+        this.setanimalName();
+        this.phylum;
+        this.withBackbone();
+        this.warmBlooded();
+    }
 
+    listProperties() {
+        console.log(this);
+    }
 }
+
+class Mammal extends Animal {
+    constructor(animalName, phylum) {
+        super(animalName, phylum);
+        this.setAnimalName();
+        this.getPhylum();
+        this.withBackbone();
+        this.warmBlooded();
+    }
+
+    listProperties() {
+        console.log(this);
+    }
+}
+
+class Arthropoda extends Animal {
+    constructor(animalName, phylum) {
+        super(animalName, phylum);
+        this.getAnimalName();
+        this.getPhylum();
+        this.withoutBackbone();
+        this.coldBlooded();
+    }
+
+    listProperties() {
+        console.log(this);
+    }
+}
+
+class Fish extends Animal {
+    constructor(animalName, phylum) {
+        super(animalName, phylum);
+        this.getAnimalName();
+        this.getPhylum();
+        this.withBackbone();
+        this.coldBlooded();
+    }
+
+    listProperties() {
+        console.log(this);
+    }
+}
+
+class Amphibia extends Animal {
+    constructor(animalName, phylum) {
+        super(animalName, phylum);
+        this.getAnimalName();
+        this.getPhylum();
+        this.withBackbone();
+        this.coldBlooded();
+    }
+
+    listProperties() {
+        console.log(this);
+    }
+}
+
+class Reptiles extends Animal {
+    constructor(animalName, phylum) {
+        super(animalName, phylum);
+        this.getAnimalName();
+        this.getPhylum();
+        this.withBackbone();
+        this.coldBlooded();
+    }
+
+    listProperties() {
+        console.log(this);
+    }
+}
+
+const aves = new Aves("Bird", "Aves");
+const mammal = new Mammal("Cat", "Mammal");
+const arthropoda = new Arthropoda("Butterfly", "Arthropoda");
+const fish = new Fish("Tilapia", "Fish");
+const amphibia = new Amphibia("Toad", "Amphibia");
+const reptiles = new Reptiles("Turtle", "Reptiles");
+
+aves.setAnimalName("Bird");
+mammal.setAnimalName("Cat");
+fish.setAnimalName("Tilapia");
+amphibia.setAnimalName("Toad");
+reptiles.setAnimalName("Turtle");
+
+console.log("\nCLASSIFICATION OF THE ANIMAL KINGDOM\n");
+
+console.log(aves.getAnimalName());
+aves.listProperties();
+
+console.log(mammal.getAnimalName());
+mammal.listProperties();
+
+arthropoda.setAnimalName();
+console.log(arthropoda.getAnimalName());
+arthropoda.listProperties();
+
+console.log(fish.getAnimalName());
+fish.listProperties();
+
+console.log(amphibia.getAnimalName());
+amphibia.listProperties();
+
+console.log(reptiles.getAnimalName());
+reptiles.listProperties();
