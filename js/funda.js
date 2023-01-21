b86f3881cf05eb877c5533f9aa5b25b9a3a07711
kingsleycj <primekings.kc@gmail.com>
2023-01-21 16:36:01 +0100

    trials

---
+++ js/funda.js
@@ -0,0 +1,12 @@
+const myLearnableObject = {name: "Ernest", age: 10}
+
+const myFavoriteCars = {
+    firstCar: "Benz",
+    secondCar: "Toyota"
+}
+const bestProgrammingLanguage = new Object {}
+bestProgrammingLanguage.firstLanguage = "Javascript"
+bestProgrammingLanguage.secondLanguage = "Python"
+
+console.log(bestProgrammingLanguage)
+
